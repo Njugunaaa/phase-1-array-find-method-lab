0342768cf501a4cec5766d25e32a660d1e8a32ba
Njuguna <joshuangich2@gmail.com>
2025-03-11 06:50:31 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+] 
+function isWin(game){
+  return game.result === "W";
+}
+function superbowlWin(record){
+  const win=record.find(isWin);
+  return win ? win.year : undefined;
+}
